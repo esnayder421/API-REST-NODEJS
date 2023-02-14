e275bf8804e2eee5aabd99f4a154a2a90794fce9
esnayder421 <esnayder421@gmail.com>
2023-02-14 12:10:56 -0500

    commit port

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 import {createPool} from 'mysql2/promise';
-import {DB_HOST,DB_USER,DB_PASSWORD,DB_PORT,DB_DATABASE} from './config.js'
+import {DB_HOST,DB_USER,DB_PASSWORD,DB_PORT,DB_DATABASE,PORT} from './config.js'
 
 export const pool = createPool({
     host: DB_HOST,
@@ -13,6 +13,10 @@ console.log(DB_USER)
 console.log(DB_PASSWORD)
 console.log(DB_PORT)
 console.log(DB_DATABASE)
+console.log('------- puerto que se esta ejecutando la api ----------------')
+console.log(PORT)
+
+
 
 
 
